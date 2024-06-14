ad323ca7af15a0a4d43a39f89206eb9896f3e05c
ImThanOs36 <lads42279@gmail.com>
2024-06-15 00:12:50 +0530

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,12 +13,12 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Blogs/>} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog/>} />
           <Route path="/publish" element={<Publish/>} />
+          <Route path="/*" element={<Blogs/>} />
         </Routes>
       </BrowserRouter>
     </>
