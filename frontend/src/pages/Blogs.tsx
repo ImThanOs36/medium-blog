0b591ad75ed66b5af65a3199677df1db682695c6
ImThanOs36 <lads42279@gmail.com>
2024-06-15 00:26:52 +0530

    initial commit

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,39 +1,58 @@
-
-import BlogCard from "../components/BlogCard"
+import { Link, useNavigate } from "react-router-dom";
 import Appbar from "../components/Appbar";
-import Loader from "../ui/Loader";
+import BlogCard from "../components/BlogCard"
 import { useBlogs } from "../hooks/useBlogs";
+import Loader from "../ui/Loader";
+
 
 
 function Blogs() {
+  const { loading, blogs, authError } = useBlogs();
+  const navigate = useNavigate()
+  const reverseBlogs = [...blogs].reverse()
+  console.log(reverseBlogs)
+  if (authError) {
+    navigate("/signin")
+  }
 
-  const { loading, blogs } = useBlogs();
-
-
-  // console.log(blog)
   return (
-    <>
-      <div>
-        <Appbar isThat={false}/>
+    <div>
 
-        <div className="flex justify-center py-20 h-full ">
-          {loading ? <Loader /> :
-            <div className="w-full max-w-screen-md  ">
-              <BlogCard
-                key={blogs.id}
-                id={blogs.id}
-                author={blogs.author.name || "Anonymous"}
-                title={blogs.title}
-                content={blogs.content}
-                createAt={blogs.createAt || "123"}
-              />
-            </div>
-          }
+      <Appbar isThat={true}/>
 
+
+      {loading ? <Loader /> :
+        <div className="flex justify-center py-20 h-full" onClick={() => {
+
+        }}>
+          <div className="w-auto max-w-4xl flex flex-col gap-10 ">
+
+
+            {reverseBlogs.map(blog => (
+
+              <Link to={`/blog/${blog.id}`} key={blog.id}>
+
+                <BlogCard
+
+                  key={blog.id}
+                  id={blog.id}
+                  author={blog.author.name || "anonymous"}
+                  title={blog.title}
+                  content={blog.content.slice(0, 200) + "..."}
+                  createAt={blog.createAt || "123"}
+
+
+                />
+
+              </Link>
+
+            ))}
+          </div>
         </div>
-      </div>
-    </>
-  )
+      }
+    </div>
+
+  );
 }
 
 export default Blogs
