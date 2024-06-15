f0b1cdc34649111556bb70faa0029abf13a28b0c
ImThanOs36 <lads42279@gmail.com>
2024-06-15 12:54:16 +0530

    Initial commit

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -26,8 +26,8 @@ function Auth({ type }: { type: "signup" | "signin" }) {
             navigate("/blogs")
         }
         catch (e: any) {
-            // console.log(e)
-            setisError(e.response.message)
+
+            setisError(e.response.data.message)
         }
 
 
@@ -35,8 +35,8 @@ function Auth({ type }: { type: "signup" | "signin" }) {
     return (
         <div className='font-satoshi'>
             <div className="mx-auto border bg-gray-50 px-4 py-4 rounded-md flex flex-col  ">
-                <div>
-                    {isError}
+                <div className='text-sm text-red-600 text-center font-bold' >
+                    {isError ? <p>Login or Password Invalid</p> : ""}
                 </div>
 
 
@@ -85,7 +85,7 @@ interface LabelInputType {
 function LabelInput({ label, placeholder, onChange, type }: LabelInputType) {
     return <div className="mb-5">
         <label htmlFor="name" className="block mb-2 text-sm font-semibold text-gray-900 ">{label}</label>
-        <input onChange={onChange} type={type} id={label} className=" border border-gray-300 text-gray-900 text-sm font-bold rounded-lg focus:ring-black focus:border-neutral-950 block w-full sm:min-w-80  py-2 px-3 " placeholder={placeholder} required />
+        <input onChange={onChange} type={type} id={label} className=" border border-gray-300 text-gray-900 text-sm font-bold rounded-lg  focus:ring-black focus:border-neutral-950 block w-full sm:min-w-80  py-2 px-3 " placeholder={placeholder} required />
     </div>
 
 }
