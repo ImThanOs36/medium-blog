c807308b4062b156843b0708881b79f13ca68a39
ImThanOs36 <lads42279@gmail.com>
2024-06-15 22:48:22 +0530

    initial commit

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -8,7 +8,7 @@ interface BlogCardProps {
     title: string,
     content: string,
     createAt: string,
-    line:boolean
+    line: boolean
 }
 
 
@@ -23,7 +23,7 @@ function BlogCard({
 
 }: BlogCardProps) {
 
-    
+
 
     return (
         <div className="font-clash_display border-b-2 border-gray-200 px-5 py-2 w-full " key={id}>
@@ -31,25 +31,25 @@ function BlogCard({
 
                 <div className="w-3/4 flex flex-col gap-6 ">
                     <h2 className="text-2xl capitalize font-semibold">{title}</h2>
-                    <p className={`font-satoshi font-medium text-gray-900 ${line? "max-h-12 overflow-hidden": ""}`}>{content.split('\n').map((line, index) => (
+                    <p className={`font-satoshi font-medium text-gray-900 min-h-20 ${line ? "max-h-20 overflow-hidden" : ""}`}>{content.split('\n').map((line, index) => (
                         <span key={index}>
                             {line}
                             <br />
                         </span>
                     ))}</p>
 
-                <Dates date={createAt} />
-            </div>
-            <div className=" sm:w-1/4 w-3/4 ">
-                <span className="text-xs font-semibold">Author</span>
-                <div className="flex sm:gap-3 gap-1  items-center">
-                    <Avatar img={false} />
-                    <div>
-                        <div className="sm:font-semibold font-medium ">{author}</div>
+                    <Dates date={createAt} />
+                </div>
+                <div className=" sm:w-1/4 w-3/4 ">
+                    <span className="text-xs font-semibold">Author</span>
+                    <div className="flex sm:gap-3 gap-1  items-center">
+                        <Avatar img={false} />
+                        <div>
+                            <div className="sm:font-semibold font-medium ">{author}</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div >
     )
 }
