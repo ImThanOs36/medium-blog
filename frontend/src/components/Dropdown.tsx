0bfa016405948f29370f7d269212a9e9efc0c541
ImThanOs36 <lads42279@gmail.com>
2024-06-16 10:18:35 +0530

    Initial commit

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Avatar from "../ui/Avatar"
+
 import { useState } from "react";
 
 function Dropdown() {
@@ -13,7 +13,13 @@ function Dropdown() {
       <div className="relative flex justify-center flex-col">
 
 
-        <button type="button" onClick={handleclick}><Avatar img={false} /></button>
+        <button type="button" onClick={handleclick}> <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+
+          <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/200.svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd">
+            </path></svg>
+
+        </div></button>
 
         {!isOpen ? null : <div id="dropdown" className="z-10 absolute top-16 -right-14   bg-black divide-y divide-gray-100 rounded-lg shadow px-1 w-40 text-center">
           <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
